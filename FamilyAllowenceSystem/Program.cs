4ad8c580b7bb013e6923846acc126fd0ed1388d2
Luis Herrera <donluisherrera@example.com>
2017-04-27 05:45:49 -0700

    completed project

--- FamilyAllowenceSystem/Program.cs
+++ FamilyAllowenceSystem/Program.cs
@@ -11,17 +11,24 @@ namespace FamilyAllowenceSystem
             var Mom = new Parent("Jessica", "woman");
             var Son1 = new Child("son", "Lloyd");
             var Son2 = new Child("son", "Webber");
+            var Daughter1 = new Child("Daughter", "Sophia");
+            var Daughter2 = new Child("Daughter", "Emilia");
+
 
             Dictionary<Child, List<Choir>> week1 = new Dictionary<Child, List<Choir>>();
             Dictionary<Child, List<Choir>> week2 = new Dictionary<Child, List<Choir>>();
 
             List<Choir> Son1ChoirsDone = new List<Choir>();
             List<Choir> Son2ChoirsDone = new List<Choir>();
+            List<Choir> Daughter1ChoirDone = new List<Choir>();
+            List<Choir> Daughter2ChoirDone = new List<Choir>();
 
+            Console.WriteLine("Week one");
             Son1ChoirsDone.Add(new Choir(ChoirName.TakeOutTrash));
             Son1ChoirsDone.Add(new Choir(ChoirName.TakeOutTrash));
+
             week1.Add(Son1, Son1ChoirsDone);
-
+            // Son2 week1 choirs
             Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
             Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
             Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
@@ -31,6 +38,27 @@ namespace FamilyAllowenceSystem
             Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
 
             week1.Add(Son2, Son2ChoirsDone);
+;
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+
+            week1.Add(Daughter1, Daughter1ChoirDone);
+
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+
+            week1.Add(Daughter2, Daughter2ChoirDone);
+
 
             foreach (KeyValuePair<Child, List<Choir>> pair in week1)
             {
@@ -41,16 +69,54 @@ namespace FamilyAllowenceSystem
                     total = total + item.getAmount();  
                 }
                 Console.WriteLine("Child: " + pair.Key.getName() + " total amount of allowence for week1: " + total.ToString());
-                
+                Console.WriteLine("");
             }
 
             Son1ChoirsDone.Clear();
+            Son2ChoirsDone.Clear();
+            Daughter1ChoirDone.Clear();
+            Daughter2ChoirDone.Clear();
+
+            Console.WriteLine("Week Two");
+
             Son1ChoirsDone.Add(new Choir(ChoirName.TakeOutTrash));
             Son1ChoirsDone.Add(new Choir(ChoirName.TakeOutTrash));
             Son1ChoirsDone.Add(new Choir(ChoirName.TakeOutTrash));
             Son1ChoirsDone.Add(new Choir(ChoirName.TakeOutTrash));
             Son1ChoirsDone.Add(new Choir(ChoirName.TakeOutTrash));
+
             week2.Add(Son1, Son1ChoirsDone);
+
+            Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
+            Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
+            Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
+            Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
+            Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
+            Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
+            Son2ChoirsDone.Add(new Choir(ChoirName.FeedBunnies));
+
+            week2.Add(Son2, Son2ChoirsDone);
+
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+            Daughter1ChoirDone.Add(new Choir(ChoirName.FeedBunnies));
+
+            week2.Add(Daughter1, Daughter1ChoirDone);
+
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+            Daughter2ChoirDone.Add(new Choir(ChoirName.Vacuum));
+
+            week2.Add(Daughter2, Daughter2ChoirDone);
+
 
             foreach (KeyValuePair<Child, List<Choir>> pair in week2)
             {
@@ -61,9 +127,8 @@ namespace FamilyAllowenceSystem
                     total = total + item.getAmount();
                 }
                 Console.WriteLine("Child: " + pair.Key.getName() + " total amount of allowence for this week2: " + total.ToString());
-
+                Console.WriteLine("");
             }
-
             Console.ReadLine();
         }
     }
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# FamilyAllowenceSystem
-# FamilyAllowenceSystem
+https://docs.google.com/document/d/1dsDnROmIJF0aN1Um_eyNbJnVpr2MLM5m6Pmj_izbW0w/edit?usp=sharing
